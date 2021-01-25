0b188d882c0205da97205e7bfd902141481d1fbe
Saber <32325484+KageYaMaa@users.noreply.github.com>
2021-01-26 00:09:08 +0800

    Delete get_names directory

--- get_names/get_names.py
+++
@@ -1,8 +0,0 @@
-def get_names(src):
-    with open(src,"r",encoding = "UTF-8") as file:
-        name_list = file.read()
-    return name_list.split("、")
-
-if __name__ == "__main__":
-    src = "../src/name_list.txt"
-    get_names(src)
